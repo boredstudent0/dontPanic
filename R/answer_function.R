1d3230349fcea42af3187839e149ea21081c2529
boredstudent0 <bored.student0@proton.me>
2024-02-19 21:08:20 +0100

    Initial commit

--- R/answer_function.R
+++ R/answer_function.R
@@ -3,13 +3,14 @@
 
 #' Answer function
 #'
-#' @param str
+#' @param str Mind the wording
 #'
 #' @return The Answer
 #' @export
 answer <- function(str){
-  distance <- attr(adist(x = str,
-                         y ="the Ulimate Question of Life, the Universe, and Everything",
+  distance <- base::attr(utils::adist(x = str,
+                         y ="the Ulimate Question of Life, the Universe,
+                         and Everything",
                          ignore.case = TRUE,
                          count = TRUE),"counts")
   if (distance[1]>distance[2]){
---
+++ tests/testthat/test-answer_function.R
@@ -0,0 +1,4 @@
+test_that("Correct wording gives the Answer", {
+  expect_equal(42, answer("the Ulimate Question of Life, the Universe,
+                         and Everything"))
+})
